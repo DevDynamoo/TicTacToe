47bd99d554e266e112cb61d5bfcd2f0fb8a0f6e2
montymse <mse450@hotmail.com>
2021-04-17 05:07:24 -0700

    Add project files.

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    partial class Form1
+    {
+        Boolean turn = true;
+        string[] game = new string[9];
+        int counter = 0;
+
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.A1 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // A1
+            // 
+            this.A1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.A1.Location = new System.Drawing.Point(15, 39);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(100, 100);
+            this.A1.TabIndex = 0;
+            this.A1.UseVisualStyleBackColor = true;
+            this.A1.Click += new System.EventHandler(this.buttonClick);
+            this.A1.MouseLeave += new System.EventHandler(this.mouseLeave);
+            this.A1.MouseHover += new System.EventHandler(this.mouseEnter);
+            // 
+            // A3
+            // 
+            this.A3.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.A3.Location = new System.Drawing.Point(227, 39);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(100, 100);
+            this.A3.TabIndex = 1;
+            this.A3.UseVisualStyleBackColor = true;
+            this.A3.Click += new System.EventHandler(this.buttonClick);
+            this.A3.MouseLeave += new System.EventHandler(this.mouseLeave);
+            this.A3.MouseHover += new System.EventHandler(this.mouseEnter);
+            // 
+            // A2
+            // 
+            this.A2.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.A2.Location = new System.Drawing.Point(121, 39);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(100, 100);
+            this.A2.TabIndex = 2;
+            this.A2.UseVisualStyleBackColor = true;
+            this.A2.Click += new System.EventHandler(this.buttonClick);
+            this.A2.MouseLeave += new System.EventHandler(this.mouseLeave);
+            this.A2.MouseHover += new System.EventHandler(this.mouseEnter);
+            // 
+            // B1
+            // 
+            this.B1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.B1.Location = new System.Drawing.Point(15, 145);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(100, 100);
+            this.B1.TabIndex = 3;
+            this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.buttonClick);
+            this.B1.MouseLeave += new System.EventHandler(this.mouseLeave);
+            this.B1.MouseHover += new System.EventHandler(this.mouseEnter);
+            // 
+            // B2
+            // 
+            this.B2.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.B2.Location = new System.Drawing.Point(121, 145);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(100, 100);
+            this.B2.TabIndex = 4;
+            this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.buttonClick);
+            this.B2.MouseLeave += new System.EventHandler(this.mouseLeave);
+            this.B2.MouseHover += new System.EventHandler(this.mouseEnter);
+            // 
+            // B3
+            // 
+            this.B3.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.B3.Location = new System.Drawing.Point(227, 145);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(100, 100);
+            this.B3.TabIndex = 5;
+            this.B3.UseVisualStyleBackColor = true;
+            this.B3.Click += new System.EventHandler(this.buttonClick);
+            this.B3.MouseLeave += new System.EventHandler(this.mouseLeave);
+            this.B3.MouseHover += new System.EventHandler(this.mouseEnter);
+            // 
+            // C1
+            // 
+            this.C1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.C1.Location = new System.Drawing.Point(15, 251);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(100, 100);
+            this.C1.TabIndex = 6;
+            this.C1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.C1.UseVisualStyleBackColor = true;
+            this.C1.Click += new System.EventHandler(this.buttonClick);
+            this.C1.MouseLeave += new System.EventHandler(this.mouseLeave);
+            this.C1.MouseHover += new System.EventHandler(this.mouseEnter);
+            // 
+            // C2
+            // 
+            this.C2.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.C2.Location = new System.Drawing.Point(121, 251);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(100, 100);
+            this.C2.TabIndex = 7;
+            this.C2.UseVisualStyleBackColor = true;
+            this.C2.Click += new System.EventHandler(this.buttonClick);
+            this.C2.MouseLeave += new System.EventHandler(this.mouseLeave);
+            this.C2.MouseHover += new System.EventHandler(this.mouseEnter);
+            // 
+            // C3
+            // 
+            this.C3.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.C3.Location = new System.Drawing.Point(227, 251);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(100, 100);
+            this.C3.TabIndex = 8;
+            this.C3.UseVisualStyleBackColor = true;
+            this.C3.Click += new System.EventHandler(this.buttonClick);
+            this.C3.MouseLeave += new System.EventHandler(this.mouseLeave);
+            this.C3.MouseHover += new System.EventHandler(this.mouseEnter);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(338, 34);
+            this.toolStrip1.TabIndex = 9;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(58, 29);
+            this.toolStripButton1.Text = "Reset";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 354);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "TicTacToe";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C3;
+        private ToolStrip toolStrip1;
+        private ToolStripButton toolStripButton1;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void buttonClick(object sender, EventArgs e)
+        {
+            try
+            {
+                Button b = (Button)sender;
+                b.Text = "X";
+                b.Enabled = false;
+
+                ButtontoBoard(b);
+                counter++;
+
+                winner();
+
+                AI();
+            }
+            catch { }
+        }
+
+        private void AI()
+        {
+            int move = maxTurn(game, 0).Item2;
+            game[move] = "O";
+            BoardtoButton(move);      
+            counter++;
+            winner();
+        }
+        private void winner()
+        {
+            if (checkgame(game, "X")) {
+                disableGame();
+                MessageBox.Show("X wins!");
+            } else if (checkgame(game,"O")) {
+                disableGame();
+                MessageBox.Show("O wins!");
+            } else if (counter == 9) {
+                MessageBox.Show("Draw");
+            }
+        }
+        private bool checkgame(string[] gameboard, string player) {
+
+            if (
+            //Horizontal
+            ((gameboard[0] == gameboard[1]) && (gameboard[1] == gameboard[2]) && gameboard[2] == player) ||
+            ((gameboard[3] == gameboard[4]) && (gameboard[4] == gameboard[5]) && gameboard[5] == player) ||
+            ((gameboard[6] == gameboard[7]) && (gameboard[7] == gameboard[8]) && gameboard[8] == player) ||
+
+            //Vertical
+            ((gameboard[0] == gameboard[3]) && (gameboard[3] == gameboard[6]) && gameboard[6] == player) ||
+            ((gameboard[1] == gameboard[4]) && (gameboard[4] == gameboard[7]) && gameboard[7] == player) ||
+            ((gameboard[2] == gameboard[5]) && (gameboard[5] == gameboard[8]) && gameboard[8] == player) ||
+
+            //Diagonal
+            ((gameboard[0] == gameboard[4]) && (gameboard[4] == gameboard[8]) && gameboard[8] == player) ||
+            ((gameboard[2] == gameboard[4]) && (gameboard[4] == gameboard[6]) && gameboard[6] == player))
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        private void disableGame()
+        {
+            foreach (Control c in Controls)
+            {
+                try
+                {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+                }
+                catch { }
+
+            }
+        }
+
+
+        //Reset button
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            counter = 0;
+            game = new string[9];
+
+            foreach (Control c in Controls)
+            {
+                try
+                {
+                    Button b = (Button)c;
+                    b.Enabled = true;
+                    b.Text = "";
+                }
+                catch { }
+
+            }
+        }
+
+
+        private void mouseEnter(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (b.Enabled)
+            {
+                b.Text = "X";
+            }
+        }
+
+        private void mouseLeave(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (b.Enabled)
+            {
+                b.Text = "";
+            }
+        }
+
+
+        private void ButtontoBoard(Button button)
+        {
+            string val = "X";
+            switch (button.Name)
+            {
+                case "A1":
+                    game[0] = val;
+                    break;
+                case "A2":
+                    game[1] = val;
+                    break;
+                case "A3":
+                    game[2] = val;
+                    break;
+                case "B1":
+                    game[3] = val;
+                    break;
+                case "B2":
+                    game[4] = val;
+                    break;
+                case "B3":
+                    game[5] = val;
+                    break;
+                case "C1":
+                    game[6] = val;
+                    break;
+                case "C2":
+                    game[7] = val;
+                    break;
+                case "C3":
+                    game[8] = val;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void BoardtoButton(int num)
+        {
+            switch (num)
+            {
+                case 0:
+                    A1.Text = "O";
+                    A1.Enabled = false;
+                    break;
+                case 1:
+                    A2.Text = "O";
+                    A2.Enabled = false;
+                    break;
+                case 2:
+                    A3.Text = "O";
+                    A3.Enabled = false;
+                    break;
+                case 3:
+                    B1.Text = "O";
+                    B1.Enabled = false;
+                    break;
+                case 4:
+                    B2.Text = "O";
+                    B2.Enabled = false;
+                    break;
+                case 5:
+                    B3.Text = "O";
+                    B3.Enabled = false;
+                    break;
+                case 6:
+                    C1.Text = "O";
+                    C1.Enabled = false;
+                    break;
+                case 7:
+                    C2.Text = "O";
+                    C2.Enabled = false;
+                    break;
+                case 8:
+                    C3.Text = "O";
+                    C3.Enabled = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private int score(string[] gameboard, int depth)
+        {
+            if (checkgame(gameboard,"O")) {
+                return  10-depth;                       // O wins
+            }
+            else if (checkgame(gameboard,"X")) {        // X wins
+                return  depth-10; 
+            }
+            return 0;                                   // Draw
+
+        }
+
+        private (int,int) maxTurn(string[] gameboard, int depth)
+        {
+            if (counter==9 || checkgame(gameboard, "X") || checkgame(gameboard,"O"))
+            {
+                return (score(gameboard, depth), -1);
+            }
+
+            (int, int) max = (int.MinValue,int.MinValue);
+            for (int i = 0; i < gameboard.Length; i++) { 
+                if (gameboard[i]==null)
+                {
+                    gameboard[i] = "O";
+                    (int, int) currentMove = minTurn(gameboard, depth+1);
+
+                    if (currentMove.Item1 > max.Item1)
+                    {
+                        max = (currentMove.Item1, i);
+                    } 
+
+                    gameboard[i] = null;
+                }
+
+            }
+            return max;
+        }
+        private (int, int) minTurn(string[] gameboard, int depth)
+        {
+            if (counter == 9 || checkgame(gameboard, "X") || checkgame(gameboard, "O"))
+            {
+                return (score(gameboard, depth), -1);
+            }
+
+            (int, int) min = (int.MaxValue,int.MaxValue);
+            for (int i = 0; i < gameboard.Length; i++)
+            {
+                if (gameboard[i] == null)
+                {
+                    gameboard[i] = "X";
+                    (int, int) currentMove = maxTurn(gameboard, depth+1);
+
+                    if (currentMove.Item1 < min.Item1)
+                    {
+                        min = (currentMove.Item1, i);
+                    }
+                    
+                    gameboard[i] = null;
+                }
+
+            }
+            return min;
+        }
+
+
+    }
+}
